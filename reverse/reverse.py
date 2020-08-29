c4956a60f5e8fcadc32ac67a76ac14806f659b49
Remon Abdou <remoo1901@hotmail.com>
2020-08-28 21:32:02 -0400

    finished reverse - MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,9 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        while(node is not None):
+            next_node = node.get_next()
+            node.next_node = prev  #reverse
+            prev = node
+            current = next_node
+        self.head = prev
